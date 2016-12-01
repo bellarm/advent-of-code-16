c044e7602dda8dced973f161aa59afa9b1a8c7a0
bellarm <rachelitabella@rocketmail.com>
2016-12-01 23:11:37 +0700

    finish day1

--- day01_2.py
+++ day01_2.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3.5
 
 steps = [tmp for tmp in input().split(', ')]
+# a bit dodgy...
+visited = [[0 for x in range(500)] for y in range(500)]
 x = 0
 y = 0
+prev_y = 0
+prev_x = 0
 dir = 'N'
 for step in steps:
+    # print(step)
+    prev_x = x
+    prev_y = y
     if step[0] == 'R':
         if dir == 'N':
             x += int(step[1:])
@@ -31,5 +38,31 @@ for step in steps:
         elif dir == 'E':
             y += int(step[1:])
             dir = 'N'
-distance = abs(x) + abs(y)
-print(distance)
+
+    if prev_y == y:
+        # just mark x
+        if x >= prev_x:
+            step = 1
+        else:
+            step = -1
+        for i in range(prev_x, x, step):
+            # print("(" + str(i) + "," + str(y) + ")")
+            if visited[250+i][250+y] == 1:
+                distance = abs(i) + abs(y)
+                print(distance)
+                # break
+            visited[250+i][250+y] = 1
+    elif prev_x == x:
+        if y >= prev_y:
+            step = 1
+        else:
+            step = -1
+        for i in range(prev_y, y, step):
+            # print("(" + str(x) + "," + str(i) + ")")
+            if visited[250+x][250+i] == 1:
+                distance = abs(i) + abs(x)
+                print(distance)
+                # break
+            visited[250+x][250+i] = 1
+    # else:
+        # print("wtf")
