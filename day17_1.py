d95f4cca8e1953992f36253187fa8e462b4a566e
bellarm <rachelitabella@rocketmail.com>
2016-12-22 02:10:40 +0700

    fix day17 part 1

--- day17_1.py
+++ day17_1.py
@@ -6,7 +6,6 @@ def is_open(c):
     return False
 
 def search_path(path, x, y):
-    global to_visit
     if x == 3 and y == 3:
         print(path)
         sys.exit()
@@ -25,5 +24,4 @@ def search_path(path, x, y):
             search_path(path+'R',x+1, y)
 passcode = "mmsxrhfx"
 path = [['' for i in range(4)] for j in range(4)]
-to_visit = [(0,0), ]
 search_path(path[0][0], 0, 0)
